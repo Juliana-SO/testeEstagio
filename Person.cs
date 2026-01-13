27ff9118436c567f9c4e0b7034247b6a14853a32
Juliana <julianaso840@gmail.com>
2026-01-13 18:18:25 -0300

    teste1

---
+++ Person.cs
@@ -0,0 +1,12 @@
+public class Person
+{
+    public String Name {get; set;}
+    public int Age {get; set;}
+    
+    public Person( String Name, int Age)
+    {
+        this.Name = Name;
+        this.Age = Age;
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(String[] args)
+    {   
+        int totalIdade = 0, i = 0, menores = 0;
+        var people = new List<Person>
+        {
+            new Person("Isaac", 19),
+            new Person("Juliana", 15),
+            new Person("Alice", 11),
+            new Person("Glaucia", 40),
+            new Person("Marcos", 3),
+        };
+        Person nova = people[0];
+        Person velha = people[0];
+
+        Console.WriteLine("Lista de Pessoas:");
+        foreach(var person in people)
+        {
+            Console.WriteLine("- " + person.Name + "  (" + person.Age + ")" );
+        }
+        Console.WriteLine();
+        foreach(var person in people)
+        {
+            if(person.Age >= 18)
+            {
+                Console.WriteLine("- " + person.Name);
+            }
+        }
+        foreach(var person in people)
+        {
+            totalIdade += person.Age;
+            i += 1;
+            if(person.Age < 18)
+            {
+                menores += 1;
+            }
+            if(person.Age > velha.Age)
+            {
+                velha = person;
+            }
+            if(person.Age < nova.Age)
+            {
+                nova = person;
+            }
+        }
+        Console.WriteLine();
+
+        double mediaIdade = (double)totalIdade / i;
+        Console.WriteLine("Média de idade: "+ mediaIdade);
+        Console.WriteLine("Pessoa mais nova: " + nova.Name + " (" + nova.Age + ")");
+        Console.WriteLine("Pessoa mais velha: " + velha.Name + " (" + velha.Age + ")");
+        Console.WriteLine("Quantidade de menores de idade: " + menores);
+    }
+}
